230f0c498f739b9a527b56634e824986cb788079
unknown <sahilshah2104@gmail.com>
2022-08-09 11:11:49 +0530

    Added code files

---
+++ colors.py
@@ -0,0 +1,27 @@
+import webcolors
+import matplotlib.pyplot as plt
+
+def closest_color(rgb) :
+    differences = {}
+    for color_hex, color_name in webcolors.CSS3_HEX_TO_NAMES.items() :
+        r, g, b = webcolors.hex_to_rgb(color_hex)
+        differences[sum([(r - rgb[0]) ** 2,
+                         (g - rgb[1]) ** 2,
+                         (b - rgb[2]) ** 2])] = color_name
+    return differences[min(differences.keys())]
+
+print("Enter the values in the range of 0 to 255")
+x = float(input("Enter the value of x :"))
+y = float(input("Enter the value of y :"))
+z = float(input("Enter the value if z :"))
+color = (x, y, z)
+
+try :
+    cname = webcolors.rgb_to_name(color)
+    print(f"The color is exactly {cname}")
+except ValueError :
+    cname = closest_color(color)
+    print(f"The color is closest to {cname}")
+
+plt.imshow([[color]])
+plt.show()
